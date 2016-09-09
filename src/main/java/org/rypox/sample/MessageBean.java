add9d02ce6a94e647b8de4ac4720524f6bf70139
jan.pfeil <jan.pfeil@rypox.net>
2016-09-09 09:03:53 +0200

    jsf index page with backing bean

---
+++ src/main/java/org/rypox/sample/MessageBean.java
@@ -0,0 +1,46 @@
+package org.rypox.sample;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class MessageBean {
+
+  private String label;
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public void onSave() {
+    System.out.println("onSave:" + this.message);
+    this.label = message;
+    this.message = message + " changed";
+  }
+
+  @PostConstruct
+  public void init() {
+    System.out.println("MessageBean.init()");
+  }
+
+  @PreDestroy
+  public void end() {
+    System.out.println("MessageBean.end()");
+  }
+}
